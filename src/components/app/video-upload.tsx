20f0fdbf997bc2c8d5ab630f8a07c8f9d4795de6
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-12-19 16:56:33 +0530

    Added File Upload

---
+++ src/components/app/video-upload.tsx
@@ -0,0 +1,120 @@
+import { cn, humanFileSize } from "@/lib/utils";
+import { type ChangeEventHandler, useRef, useState } from "react";
+import { Card, CardContent, CardFooter } from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Icons } from "@/components/app/icons";
+import Image from "next/image";
+
+export const FileInfo: React.FC<{
+  file: File;
+}> = ({ file }) => {
+  let fileType: "other" | "pdf" | "image" | "video" = "other";
+
+  console.log(file.type);
+
+  if (file.type.includes("pdf")) fileType = "pdf";
+  else if (file.type.includes("image")) fileType = "image";
+  else if (file.type.includes("video")) fileType = "video";
+
+  return (
+    <div className="flex w-full flex-row gap-4">
+      <div className="relative flex h-20 w-28 items-center justify-center rounded-lg bg-slate-100 dark:border dark:bg-card/30">
+        {fileType === "pdf" && <Icons.fileIcon />}
+
+        {fileType === "image" && (
+          <Image
+            src={URL.createObjectURL(file)}
+            alt="Image"
+            fill
+            className="mx-auto max-w-[95%] rounded-lg object-contain"
+          />
+        )}
+
+        {fileType === "other" && <Icons.fileTextIcon />}
+        {fileType === "video" && <Icons.check />}
+      </div>
+
+      <div className="flex grow flex-col gap-1">
+        <p className="text-md font-bold">{file.name}</p>
+        <p>{humanFileSize(file.size)}</p>
+      </div>
+    </div>
+  );
+};
+
+export const FileUploaderDroppable: React.FC<{
+  className?: string;
+  file: File | null;
+  onFilesSet: React.Dispatch<React.SetStateAction<File | null>>;
+}> = ({ className = "", file, onFilesSet }) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const onUploadClick = () => {
+    inputRef.current?.click();
+  };
+
+  const onChange: ChangeEventHandler<HTMLInputElement> = (event) => {
+    const files = event.target.files;
+
+    if (files && files.length > 0) {
+      onFilesSet(files[0] ?? null);
+    }
+  };
+
+  const onClear = () => {
+    if (inputRef.current?.value) {
+      inputRef.current.value = "";
+      onFilesSet(null);
+    }
+  };
+
+  return (
+    <Card className={cn(className)}>
+      {/* <CardHeader>
+        <CardTitle className="text-md">{title}</CardTitle>
+        <CardDescription>{description}</CardDescription>
+      </CardHeader> */}
+
+      <CardContent>
+        <div className="mt-5 flex h-48 w-full items-center justify-center rounded-lg border border-dashed bg-slate-50 dark:bg-card/30">
+          <Button
+            type="button"
+            variant="link"
+            className="h-16"
+            onClick={onUploadClick}
+          >
+            Drag and drop or Choose your files
+          </Button>
+        </div>
+
+        <Input
+          type="file"
+          multiple={false}
+          className="hidden"
+          ref={inputRef}
+          onChange={onChange}
+          accept="video/mp4,video/x-m4v,video/*"
+        />
+
+        {file && (
+          <div>
+            <div className="h-4"></div>
+            <div className="h-auto max-h-[400px] w-auto max-w-[600px]">
+              <video src={URL.createObjectURL(file)} className=""></video>
+            </div>
+          </div>
+        )}
+      </CardContent>
+
+      <CardFooter className="flex justify-start gap-2">
+        <Button variant="default" type="button" onClick={onClear}>
+          Process
+        </Button>
+        <Button variant="outline" type="button" onClick={onClear}>
+          Clear
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
 import Head from "next/head";
-import Link from "next/link";
+import { useState } from "react";
+import { FileUploaderDroppable } from "~/components/app/video-upload";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from "~/components/ui/card";
 
 export default function Home() {
+  const [video, setVideo] = useState<File | null>(null);
+
   return (
     <>
       <Head>
@@ -9,34 +18,23 @@ export default function Home() {
         <meta name="description" content="Video to Text" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
       <main className="flex min-h-screen flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
-          <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
-            Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+          <h1 className="bg-gradient-to-r from-blue-400 to-destructive bg-clip-text text-5xl font-extrabold tracking-tight text-transparent sm:text-[5rem]">
+            Video to Text
           </h1>
+
           <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/usage/first-steps"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">First Steps →</h3>
-              <div className="text-lg">
-                Just the basics - Everything you need to know to set up your
-                database and authentication.
-              </div>
-            </Link>
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 text-white hover:bg-white/20"
-              href="https://create.t3.gg/en/introduction"
-              target="_blank"
-            >
-              <h3 className="text-2xl font-bold">Documentation →</h3>
-              <div className="text-lg">
-                Learn more about Create T3 App, the libraries it uses, and how
-                to deploy it.
-              </div>
-            </Link>
+            <FileUploaderDroppable file={video} onFilesSet={setVideo} />
+
+            <div>
+              <Card className="h-full">
+                <CardHeader></CardHeader>
+                <CardContent>knknkn</CardContent>
+                <CardFooter></CardFooter>
+              </Card>
+            </div>
           </div>
         </div>
       </main>
