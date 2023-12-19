107d98d07a21d788b0e2db59835a26e2ef438f0b
Prathamesh Mutkure <pmutkure009@gmail.com>
2023-12-19 16:56:24 +0530

    Added Shadcn UI

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,23 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function formatDate(date: Date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+  const formattedDate = `${year}-${month}-${day}`;
+
+  return formattedDate;
+}
+
+export function humanFileSize(size: number) {
+  const i = size == 0 ? 0 : Math.floor(Math.log(size) / Math.log(1024));
+  const value = size / Math.pow(1024, i);
+  const valueFixed = value.toFixed(2);
+
+  return valueFixed + " " + ["B", "kB", "MB", "GB", "TB"][i];
 }
